66929c65fc7d98b429cf0a62150cc1a21bfd5fce
Vic P <vic4key@gmail.com>
2022-11-11 03:11:35 +0700

    Initial

--- main.py
+++ main.py
@@ -132,6 +132,8 @@ def test():
 		phone_numbers = extract_phone_numbers(text)
 		assert len(phone_numbers) == 1 and phone_numbers[0] == "0981234567", text
 
+	print("ALL TEST CASES ARE PASSED")
+
 if __name__ == "__main__":
 	test()
 	pass
